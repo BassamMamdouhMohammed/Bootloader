9667f236c453b46c2cc39e8229e7579af219356b
BassamMamdouhMohammed <bassammamdouhmohammed@gmail.com>
2023-06-22 12:05:13 +0300

    add Flash_Erase & Jump_ToAddress functionality

--- Bootloader/bootloader.c
+++ Bootloader/bootloader.c
@@ -7,34 +7,10 @@
  * Version: 1.0.0
  */
 
-
+/*--------------------------- INCLUDES --------------------------------------*/
 #include "Bootloader.h"
-
+/*--------------------------- Global Variables Definitions ------------------*/
 static uint8_t BL_HostBuffer[BL_HOST_BUFFER_RX_LENGTH];
-
-/**
-*
-*
-*/
-static void Bootloader_GetVersion(uint8_t *HostBuffer);
-static void Bootloader_GetHelp(uint8_t *HostBuffer);
-static void Bootloader_GetSectorProtectionStatus(uint8_t *HostBuffer);
-static void Bootloader_GetChip_IdentificationNumber(uint8_t *HostBuffer);
-
-static void Bootloader_JumpToAddress(uint8_t *HostBuffer);
-static void Bootloader_EraseFlash(uint8_t *HostBuffer);
-static void Bootloader_MemoryWrite(uint8_t *HostBuffer);
-static void Bootloader_MemoryRead(uint8_t *HostBuffer);
-
-static void Bootloader_ReadProtectionLevel(uint8_t *HostBuffer);
-static void Bootloader_Enable_RW_Protection(uint8_t *HostBuffer);
-static void Bootloader_Disable_RW_Protection(uint8_t *HostBuffer);
-
-static void Bootloader_UART_Transmit(uint8_t *HostBuffer,uint8_t msgLength);
-
-static uint8_t Bootloader_CRC_verify(uint8_t *Data, uint32_t DataLength, uint32_t Host_CRC);
-static void Bootloader_Send_Ack(uint8_t msgLength);
-static void Bootloader_Send_NAck();
 static uint8_t Bootloader_Supported_CMDs[12] = 
 	{
 		BL_GET_VERSION_CMD,
@@ -50,6 +26,29 @@ static uint8_t Bootloader_Supported_CMDs[12] =
 		BL_READ_OTP_CMD,
 		BL_DIS_RW_PROTECT_CMD,
 	};
+/*--------------------------- Static Functions Prototypes -------------------*/
+static void Bootloader_GetVersion(uint8_t *HostBuffer);
+static void Bootloader_GetHelp(uint8_t *HostBuffer);
+static void Bootloader_GetSectorProtectionStatus(uint8_t *HostBuffer);
+static void Bootloader_GetChip_IdentificationNumber(uint8_t *HostBuffer);
+
+static void Bootloader_JumpToAddress(uint8_t *HostBuffer);
+static void Bootloader_EraseFlash(uint8_t *HostBuffer);
+static void Bootloader_MemoryWrite(uint8_t *HostBuffer);
+static void Bootloader_MemoryRead(uint8_t *HostBuffer);
+
+static void Bootloader_ReadProtectionLevel(uint8_t *HostBuffer);
+static void Bootloader_Enable_RW_Protection(uint8_t *HostBuffer);
+static void Bootloader_Disable_RW_Protection(uint8_t *HostBuffer);
+
+/*------------------------- static helper functions -----------------------------*/
+static void Bootloader_UART_Transmit(uint8_t *HostBuffer,uint8_t msgLength);
+static void Bootloader_JumpToUserApplication(void);
+static uint8_t Bootloader_CRC_verify(uint8_t *Data, uint32_t DataLength, uint32_t Host_CRC);
+static void Bootloader_Send_Ack(uint8_t msgLength);
+static void Bootloader_Send_NAck();
+static uint8_t Bootloader_JumpAddressVerify(uint32_t address);
+/*-------------------------- Bootloader functionality Implementatoin ------------------*/
 void BL_PrintMessage(char *format, ...)
 {
 	char Message[100] = {0};
@@ -66,7 +65,7 @@ void BL_PrintMessage(char *format, ...)
 	va_end(args);
 
 }
-
+/*--------------------------- Static Functions Definitions -------------------*/
 BL_STATUS BL_UART_FetchHostCommand(void)
 {
 	BL_STATUS Status = BL_OK;
@@ -82,106 +81,71 @@ BL_STATUS BL_UART_FetchHostCommand(void)
 	{
 		DataLength = BL_HostBuffer[0];
 		HAL_Status = HAL_UART_Receive(BL_HOST_COMMUNICATION_UART,&BL_HostBuffer[1],DataLength,HAL_MAX_DELAY);
-		if(HAL_ERROR == HAL_Status)
+		if(HAL_OK != HAL_Status)
 		{
 			BL_PrintMessage("HAL_ERROR happend 2section \r\n");
+			HAL_Status = HAL_ERROR;
 		}
 		else
 		{
 			switch(BL_HostBuffer[1])
 			{
 				case BL_GET_VERSION_CMD:
-					
-					break;
+					Bootloader_GetVersion(BL_HostBuffer);
+				break;
 				case BL_GET_HELP_CMD:
-			
-					break;
+					Bootloader_GetHelp(BL_HostBuffer);
+				break;
 				case BL_GET_CID_CMD:
-					BL_PrintMessage("BL_GET_CID_CMD \r\n");
-					break;
+					Bootloader_GetChip_IdentificationNumber(BL_HostBuffer);
+				break;
 				case BL_GET_RDP_STATUS_CMD:
 					BL_PrintMessage("BL_GET_RDP_STATUS_CMD \r\n");
-					break;
+				break;
 				case BL_GO_TO_ADDRESS_CMD:
-					BL_PrintMessage("BL_GO_TO_ADDRESS_CMD \r\n");
-					break;
+					Bootloader_JumpToAddress(BL_HostBuffer);
+				break;
 				case BL_FLASH_ERASE_CMD:
-					BL_PrintMessage("BL_FLASH_ERASE_CMD \r\n");
-					break;
+					Bootloader_EraseFlash(BL_HostBuffer);
+				break;
 				case BL_MEM_WRITE_CMD:
 					BL_PrintMessage("BL_MEM_WRITE_CMD \r\n");
-					break;
+				break;
 				case BL_EN_RW_PROTECT_CMD:
 					BL_PrintMessage("BL_EN_RW_PROTECT_CMD \r\n");
-					break;
+				break;
 				case BL_MEM_READ_CMD:
 					BL_PrintMessage("BL_MEM_READ_CMD \r\n");
-					break;
+				break;
 				case BL_READ_SECTOR_STATUS_CMD:
 					BL_PrintMessage("BL_READ_SECTOR_STATUS_CMD \r\n");
-					break;
+				break;
 				case BL_READ_OTP_CMD:
 					BL_PrintMessage("BL_READ_OTP_CMD \r\n");
-					break;
+				break;
 				case BL_DIS_RW_PROTECT_CMD:
 					BL_PrintMessage("BL_DIS_RW_PROTECT_CMD \r\n");
-					break;
+				break;
 				default: BL_PrintMessage("Invalid Bootloader Command \r\n");
-					break;
+				break;
 			}
 		}
 	}
 	return Status;
 }
 
-static void Bootloader_Send_Ack(uint8_t msgLength)
-{
-	uint8_t AckValue[2] = {0};
-	AckValue[0] = BL_ACK;
-	AckValue[1] = msgLength;
-	HAL_UART_Transmit(BL_HOST_COMMUNICATION_UART,(uint8_t *)AckValue,2,HAL_MAX_DELAY);
-}
-
-static void Bootloader_Send_NAck()
-{
-	uint8_t NACKvalue = BL_NACK;
-	HAL_UART_Transmit(BL_HOST_COMMUNICATION_UART,&NACKvalue,1,HAL_MAX_DELAY);
-}
-
-static uint8_t Bootloader_CRC_verify(uint8_t *Data, uint32_t DataLength, uint32_t Host_CRC)
-{
-	uint8_t CRC_Status = CRC_PASSED;
-	uint32_t BL_calc_CRC = 0;
-	uint8_t Iterator = 0;
-	uint32_t DataBuffer = 0;
-	for(Iterator = 0; Iterator < DataLength; ++Iterator){
-		DataBuffer = (uint32_t)Data[Iterator];
-		BL_calc_CRC = HAL_CRC_Accumulate(BL_CRC_HANDLE,&DataBuffer,1);
-	}
-	__HAL_CRC_DR_RESET(BL_CRC_HANDLE);
-	
-	if(Data == NULL || (BL_calc_CRC != Host_CRC)){
-		CRC_Status = CRC_FAILED;
-	} else {}
-		
-	return CRC_Status;
-}
-
-static void Bootloader_UART_Transmit(uint8_t *HostBuffer,uint8_t msgLength)
-{
-	HAL_UART_Transmit(BL_HOST_COMMUNICATION_UART,HostBuffer,msgLength,HAL_MAX_DELAY);
-}
 
 static void Bootloader_GetVersion(uint8_t *HostBuffer)
 {
 #if (BL_INFO_DEBUG == BL_DEBUG_ENABLE)
-	BL_PrintMessage("BL_GET_VERSION_CMD \r\n");
+	BL_PrintMessage("Read Bootloader version from MCU \r\n");
 #endif	
+	uint16_t HostPacketLen = HostBuffer[0] + 1;
+	/* assigns the value of the crc32 to HostCRC */
+	uint32_t HostCRC = *((uint32_t *)((HostBuffer + HostPacketLen) - BL_CRC_SIZE_BYTES));
 	uint8_t BootloaderVersion[4] = {BL_VENDOR_ID,BL_SW_MAJOR_VERSION,
 									BL_SW_MINOR_VERSION,BL_SW_PATCH_VERSION};
-	uint16_t HostPacketLen = HostBuffer[0];
-	uint32_t HostCRC = *((uint32_t *)((HostBuffer + HostPacketLen) - BL_CRC_SIZE_BYTES));
-									
+	/* CRC32 verification checking */
 	if(CRC_PASSED == Bootloader_CRC_verify((uint8_t *)&HostBuffer[0],HostPacketLen - 4,HostCRC))
 	{
 #if (BL_INFO_DEBUG == BL_DEBUG_ENABLE)
@@ -191,7 +155,7 @@ static void Bootloader_GetVersion(uint8_t *HostBuffer)
 		Bootloader_UART_Transmit((uint8_t *)&BootloaderVersion[0],4);
 #if (BL_INFO_DEBUG == BL_DEBUG_ENABLE)
 	BL_PrintMessage("Bootloader Version. %d.%d.%d \r\n",
-		                                   BootloaderVersion[0],BootloaderVersion[1],BootloaderVersion[2]);
+		                                   BootloaderVersion[1],BootloaderVersion[2],BootloaderVersion[3]);
 #endif			
 	}
 	else{
@@ -204,10 +168,12 @@ static void Bootloader_GetVersion(uint8_t *HostBuffer)
 static void Bootloader_GetHelp(uint8_t *HostBuffer)
 {
 #if (BL_INFO_DEBUG == BL_DEBUG_ENABLE)
-	BL_PrintMessage("BL_GET_HELP_CMD \r\n");
+	BL_PrintMessage("list commands supported by bootloader \r\n");
 #endif		
+	/* calculate CRC32 */
 	uint16_t HostPacketLen = HostBuffer[0];
 	uint32_t HostCRC = *((uint32_t *)((HostBuffer + HostPacketLen) - BL_CRC_SIZE_BYTES));
+	/* checking CRC32 */
 	if(CRC_PASSED == Bootloader_CRC_verify((uint8_t *)&HostBuffer[0],HostPacketLen - 4,HostCRC))
 	{
 #if (BL_INFO_DEBUG == BL_DEBUG_ENABLE)
@@ -227,17 +193,20 @@ static void Bootloader_GetHelp(uint8_t *HostBuffer)
 static void Bootloader_GetChip_IdentificationNumber(uint8_t *HostBuffer)
 {
 #if (BL_INFO_DEBUG == BL_DEBUG_ENABLE)
-	BL_PrintMessage("BL_GET_HELP_CMD \r\n");
+	BL_PrintMessage("Read MCU chip Identification number \r\n");
 #endif		
+	/* calculate CRC32 */
 	uint16_t HostPacketLen = HostBuffer[0];
 	uint32_t HostCRC = *((uint32_t *)((HostBuffer + HostPacketLen) - BL_CRC_SIZE_BYTES));
 	uint16_t MCU_ID = 0;
+	/* checking CRC32 */
 	if(CRC_PASSED == Bootloader_CRC_verify((uint8_t *)&HostBuffer[0],HostPacketLen - 4,HostCRC))
 	{
 #if (BL_INFO_DEBUG == BL_DEBUG_ENABLE)
 	BL_PrintMessage("CRC Verification Passed \r\n");
 #endif	
 		MCU_ID = (uint16_t)(DBGMCU->IDCODE & 0x00000FFF);
+		/* Report chip identification number to HOST */
 		Bootloader_Send_Ack(2);
 		Bootloader_UART_Transmit((uint8_t *)&MCU_ID,2);
 	}
@@ -249,19 +218,75 @@ static void Bootloader_GetChip_IdentificationNumber(uint8_t *HostBuffer)
 	}	
 }
 
-static void Bootloader_GetSectorProtectionStatus(uint8_t *HostBuffer)
-{
-	
-}
-
-
 static void Bootloader_JumpToAddress(uint8_t *HostBuffer)
 {
-	
+	uint16_t HostPacketLen = 0;
+	uint32_t HostCRC = 0;
+	uint32_t HostJumpAddress = 0x00;
+	uint8_t AddrStatus = ADDRESS_VALID; 
+#if (BL_INFO_DEBUG == BL_DEBUG_ENABLE)
+	BL_PrintMessage("Jump Bootloader to Specific Address \r\n");
+#endif
+	/* calculate CRC32 */
+	 HostPacketLen = HostBuffer[0];
+	 HostCRC = *((uint32_t *)((HostBuffer + HostPacketLen) - BL_CRC_SIZE_BYTES));
+	/* checking CRC32 */
+	if(CRC_PASSED == Bootloader_CRC_verify((uint8_t *)&HostBuffer[0],HostPacketLen - 4,HostCRC))
+	{
+#if (BL_INFO_DEBUG == BL_DEBUG_ENABLE)
+	BL_PrintMessage("CRC Verification Passed \r\n");
+#endif
+		Bootloader_Send_Ack(1);
+		HostJumpAddress = *((uint32_t *)&HostBuffer[2]);
+		AddrStatus = Bootloader_JumpAddressVerify(HostJumpAddress);
+		if(ADDRESS_VALID == AddrStatus)
+		{
+#if (BL_INFO_DEBUG == BL_DEBUG_ENABLE)
+	BL_PrintMessage("Address Verification Passed \r\n");
+#endif			
+			Bootloader_UART_Transmit((uint8_t *)&AddrStatus, 1);
+			/* Prepare the address to jump */
+			JumpPTR JumpAddr = (JumpPTR)(HostJumpAddress + 1); 
+#if (BL_INFO_DEBUG == BL_DEBUG_ENABLE)
+	BL_PrintMessage("Jump to : 0x%X \r\n",JumpAddr);
+#endif
+		JumpAddr();
+		}
+		else
+		{
+			Bootloader_UART_Transmit((uint8_t *)&AddrStatus, 1);
+		}
+	}
+	else
+	{
+#if (BL_INFO_DEBUG == BL_DEBUG_ENABLE)
+	BL_PrintMessage("CRC Verification Failed \r\n");
+#endif	
+		Bootloader_Send_NAck();
+	}
 }
 static void Bootloader_EraseFlash(uint8_t *HostBuffer)
 {
-	
+	uint16_t HostPacketLen = 0;
+	uint32_t HostCRC = 0;
+#if (BL_INFO_DEBUG == BL_DEBUG_ENABLE)
+	BL_PrintMessage("Mass erase or sector erase of the user flash \r\n");
+#endif
+	/* calculate CRC32 */
+	 HostPacketLen = HostBuffer[0];
+	 HostCRC = *((uint32_t *)((HostBuffer + HostPacketLen) - BL_CRC_SIZE_BYTES));
+	/* checking CRC32 */
+	if(CRC_PASSED == Bootloader_CRC_verify((uint8_t *)&HostBuffer[0],HostPacketLen - 4,HostCRC))
+	{
+#if (BL_INFO_DEBUG == BL_DEBUG_ENABLE)
+	BL_PrintMessage("CRC Verification Passed \r\n");
+#endif
+		Bootloader_Send_Ack(1);	
+	}
+	else
+	{
+		
+	}
 }
 static void Bootloader_MemoryWrite(uint8_t *HostBuffer)
 {
@@ -284,5 +309,141 @@ static void Bootloader_Disable_RW_Protection(uint8_t *HostBuffer)
 {
 	
 }
+static void Bootloader_GetSectorProtectionStatus(uint8_t *HostBuffer)
+{
+	
+}
 
+/*-------------------------- static heper funtions Definition -------------------------*/
+static void Bootloader_JumpToUserApplication(void)
+{
+	/* get main stack pionter value of main application */
+	uint32_t MSP_Value = *((volatile uint32_t *) FLASH_SECTOR2_BASE_ADDRESS);
+	/* reset handler definition funtion of main application */
+	uint32_t MainAppAddr = *((volatile uint32_t *)(FLASH_SECTOR2_BASE_ADDRESS + 4));
+	/* Fetch the reset handler address of the user application */
+	pMainApp ResetHandlerAddr = (pMainApp)MainAppAddr;
+	/* Assigns the given value to the Main Stack Pointer */
+	__set_MSP(MSP_Value);
+	/* Resets the RCC clock configuration to the default reset state. */
+	HAL_RCC_DeInit();
+	/* Jump to Application Reset Handler */
+	ResetHandlerAddr();
+}
 
+static void Bootloader_Send_Ack(uint8_t msgLength)
+{
+	uint8_t AckValue[2] = {0};
+	AckValue[0] = BL_ACK;
+	AckValue[1] = msgLength;
+	HAL_UART_Transmit(BL_HOST_COMMUNICATION_UART,(uint8_t *)AckValue,2,HAL_MAX_DELAY);
+}
+
+static void Bootloader_Send_NAck()
+{
+	uint8_t NACKvalue = BL_NACK;
+	HAL_UART_Transmit(BL_HOST_COMMUNICATION_UART,&NACKvalue,1,HAL_MAX_DELAY);
+}
+
+static uint8_t Bootloader_CRC_verify(uint8_t *Data, uint32_t DataLength, uint32_t Host_CRC)
+{
+	uint8_t CRC_Status = CRC_PASSED;
+	uint32_t BL_calc_CRC = 0;
+	uint8_t Iterator = 0;
+	uint32_t DataBuffer = 0;
+	/* calulate CRC32 */
+	if(NULL != Data)
+	{
+		for(Iterator = 0; Iterator < DataLength; ++Iterator){
+		DataBuffer  = (uint32_t)Data[Iterator];
+		BL_calc_CRC = HAL_CRC_Accumulate(BL_CRC_HANDLE,&DataBuffer,1);
+		}
+		/* Reset the CRC Calculation Unit */
+		__HAL_CRC_DR_RESET(BL_CRC_HANDLE);
+		/* check if the calculated crc is equal to host crc(python script) */
+		if(BL_calc_CRC != Host_CRC){
+			CRC_Status = CRC_FAILED;
+		} else {}
+	}
+	else
+	{
+		CRC_Status = NULL_POINTER;
+	}
+	return CRC_Status;
+}
+
+static void Bootloader_UART_Transmit(uint8_t *HostBuffer,uint8_t msgLength)
+{
+	HAL_UART_Transmit(BL_HOST_COMMUNICATION_UART,HostBuffer,msgLength,HAL_MAX_DELAY);
+}
+static uint8_t Bootloader_JumpAddressVerify(uint32_t address)
+{
+	uint8_t addr_status = ADDRESS_VALID;
+	if((address >= SRAM1_BASE) && (address <= NUCLEO_F446RE_SRAM1_END)){}
+	else if((address >= SRAM2_BASE) && (address <= NUCLEO_F446RE_SRAM2_END)){}
+	else if((address >= SRAM1_BASE) && (address <= NUCLEO_F446RE_SRAM1_END)){}	
+	else if((address >= FLASH_BASE) && (address <= NUCLEO_F446RE_FLASH_END)){}	
+	else
+	{
+		addr_status = ADDRESS_INVALID;
+	}
+	return addr_status;
+}
+static uint8_t FlashErase(uint8_t sectorNb, uint8_t Nbofsectors)
+{
+	uint8_t sectorStatus = SECTOR_VALID;
+	FLASH_EraseInitTypeDef pEraseInit;
+	uint8_t rem_sectors = 0;
+	uint32_t SectorError = 0;
+	if(Nbofsectors < FLASH_MAX_SECTOR_NB)
+	{
+		if((sectorNb <= (FLASH_MAX_SECTOR_NB - 1)) || (FLASH_MASS_ERASE == sectorNb))
+		{
+			if(FLASH_MASS_ERASE == sectorNb)
+			{
+				pEraseInit.TypeErase = FLASH_TYPEERASE_MASSERASE;
+#if (BL_INFO_DEBUG == BL_DEBUG_ENABLE)
+				BL_PrintMessage("Flash Mass Erase activation \r\n");
+#endif
+			}
+			else
+			{
+#if (BL_INFO_DEBUG == BL_DEBUG_ENABLE)
+				BL_PrintMessage("Sector Erase activation \r\n");
+#endif			
+				rem_sectors = FLASH_MAX_SECTOR_NB - sectorNb;
+				if(Nbofsectors > rem_sectors)
+				{
+					Nbofsectors = rem_sectors;
+				}else{}
+				pEraseInit.TypeErase = FLASH_TYPEERASE_SECTORS; /* Sectors erase only */
+				pEraseInit.Sector = sectorNb;        /* Initial FLASH sector to erase when Mass erase is disabled */
+				pEraseInit.NbSectors = Nbofsectors; /* Number of sectors to be erased. */					
+			}
+			pEraseInit.Banks = FLASH_BANK_1; /* Bank 1  */
+			pEraseInit.VoltageRange = FLASH_VOLTAGE_RANGE_3; /* Device operating range: 2.7V to 3.6V */		
+			/* Unlock the FLASH control register access */
+			HAL_FLASH_Unlock();		
+			/* Perform a mass erase or erase the specified FLASH memory sectors */
+			HAL_FLASHEx_Erase(&pEraseInit, &SectorError);	
+			if(HAL_SUCCESSFUL_ERASE == SectorError)
+			{
+				sectorStatus = SUCCESSFUL_ERASE;
+			}				
+			else
+			{
+				sectorStatus = FAILED_ERASE;
+			}
+			HAL_FLASH_Lock();
+		}
+		else
+		{
+			sectorStatus = SECTOR_INVALID;
+		}
+	}
+	else
+	{
+		sectorStatus = SECTOR_INVALID;
+	}
+	return sectorStatus;
+}
--- Bootloader/bootloader.h
+++ Bootloader/bootloader.h
@@ -19,21 +19,24 @@
 #include "usart.h"
 #include "crc.h"
 /*---------------------------------- MACRO DEFINITION -------------------------------- */
-#define BL_DEBUG_UART                  (&huart2)
-#define BL_HOST_COMMUNICATION_UART     (&huart1)
-#define BL_ENABLE_DEBUG_METHOD         (BL_ENABLE_UART_DEBUG_MESSAGE)
-#define BL_CRC_HANDLE                  (&hcrc)
-#define BL_HOST_BUFFER_RX_LENGTH       (200)
+#define BL_ENABLE_UART_DEBUG_MESSAGE   (0x00)
+/* Enable & Disable debugging info in any terminal ex.RealTerm */
 #define BL_DEBUG_ENABLE                (0x01)
 #define BL_DEBUG_DISABLE               (0x00)
+/* Communication protocol used to transmit and receive commands*/
+#define BL_DEBUG_UART                  (&huart2) 
+#define BL_HOST_COMMUNICATION_UART     (&huart1)
+#define BL_CRC_HANDLE                  (&hcrc)
+#define BL_ENABLE_DEBUG_METHOD         (BL_ENABLE_UART_DEBUG_MESSAGE)
 #define BL_INFO_DEBUG                  (BL_DEBUG_ENABLE)
-#define BL_ENABLE_UART_DEBUG_MESSAGE   (0x00)
-#define BL_ENABLE_SPI_DEBUG_MESSAGE    (0x01)
-#define BL_ENABLE_CAN_DEBUG_MESSAGE    (0x02)
-
+/* Buffer to receive bootloader frame info in Bytes */
+#define BL_HOST_BUFFER_RX_LENGTH       (200)
+/*Bootloader supported commands to perform various operations */
+/* Generic commands (bootloader info) */
 #define BL_GET_VERSION_CMD             (0x10)
 #define BL_GET_HELP_CMD                (0x11)
 #define BL_GET_CID_CMD                 (0x12)
+
 #define BL_GET_RDP_STATUS_CMD          (0x13)
 #define BL_GO_TO_ADDRESS_CMD           (0x14)
 #define BL_FLASH_ERASE_CMD             (0x15)
@@ -43,23 +46,47 @@
 #define BL_READ_SECTOR_STATUS_CMD      (0x19)
 #define BL_READ_OTP_CMD                (0x20)
 #define BL_DIS_RW_PROTECT_CMD          (0x21)
-
+/* bootloader version info */
 #define BL_VENDOR_ID                    (127)
 #define BL_SW_MAJOR_VERSION             (1)
 #define BL_SW_MINOR_VERSION             (0)
 #define BL_SW_PATCH_VERSION             (0)
-
+/* crc32 size in bootloader frame & it's expected conditions */
 #define BL_CRC_SIZE_BYTES               (4)
 #define CRC_FAILED                      (0x00)
 #define CRC_PASSED                      (0x01)
+/* bootloader acknowledge */
 #define BL_ACK                          (0xAB)
 #define BL_NACK                         (0xCD)
+
+#define NULL_POINTER                    (0x00)
+/* jump to address conditions for validaty */
+#define ADDRESS_VALID                   (0x01)
+#define ADDRESS_INVALID                 (0x00)
+#define SECTOR_VALID                    (0x01) /* Erase Flash Memory conditions */
+#define SECTOR_INVALID                  (0x01)
+#define FLASH_MAX_SECTOR_NB             (0x07) /* STM32F446RE Number Of Sectors */
+#define FLASH_MASS_ERASE                (0xFF)
+#define SUCCESSFUL_ERASE                (0x03)
+#define FAILED_ERASE                    (0x02)
+
+#define HAL_SUCCESSFUL_ERASE            (0xFFFFFFFFU)
+#define NUCLEO_F446RE_SRAM1_SIZE        (112 * 1024)
+#define NUCLEO_F446RE_SRAM2_SIZE        (16 * 1024)
+#define NUCLEO_F446RE_FLASH_SIZE        (1024 * 1024)
+#define NUCLEO_F446RE_SRAM1_END         (SRAM1_BASE + NUCLEO_F446RE_SRAM1_SIZE)
+#define NUCLEO_F446RE_SRAM2_END         (SRAM2_BASE + NUCLEO_F446RE_SRAM2_SIZE)
+#define NUCLEO_F446RE_FLASH_END         (FLASH_BASE + NUCLEO_F446RE_FLASH_SIZE)
+
+#define FLASH_SECTOR2_BASE_ADDRESS      (0x08008000U) /* Start address for App.c */
+typedef void (*pMainApp)(void);
+typedef void (*JumpPTR)(void);
 /*---------------------------------- MACRO FUNCTION ---------------------------------- */
 
 /*---------------------------------- USER DATA TYPE ---------------------------------- */
 typedef enum{
-	BL_ERROR = 0,
-	BL_OK
+    BL_ERROR = 0,
+    BL_OK
 }BL_STATUS;
 /*---------------------------------- APIs PROTOTYPE ---------------------------------- */
 void BL_PrintMessage(char *format, ...);
